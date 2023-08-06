1bca5c686fd2c007a61e941aaf44fa54ce84615e
Alinaswe <85223818+Alinaswe3@users.noreply.github.com>
2023-08-06 18:27:53 +0200

    Added default css configs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,15 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'light-gray': 'hsl(0, 0%, 17%)',
+				'dark-gray': 'hsl(0, 0%, 59%)'
+			},
+			fontSize: {
+				body: '1.8rem'
+			}
+		}
 	},
 
 	plugins: []
