0a3a66837abfd9b718c1a488f986cf86bdc12e11
Alinaswe <85223818+Alinaswe3@users.noreply.github.com>
2023-08-08 00:16:26 +0200

    Built the api endpoint

--- src/routes/api/location/+server.ts
+++ src/routes/api/location/+server.ts
@@ -4,12 +4,24 @@ import { json } from '@sveltejs/kit';
 const GET_USER_IP_API_URL = 'https://api.ipify.org?format=json';
 const USER_SEARCH_IP_API_URL = `https://geo.ipify.org/api/v2/country?apiKey=${API_KEY}`;
 
-export function GET() {
-	return new Response(JSON.stringify('APi is working'));
-}
+const DOMAIN_REGEX = /^([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}$/g;
+const IP_ADDRESS_REGEX =
+	/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/g;
 
 export async function POST({ request }) {
-	const data = await request.json();
-	console.log(data);
-	return json({ message: 'Test successful' });
+	const { address } = await request.json();
+
+	if (address.match(IP_ADDRESS_REGEX)) {
+		const res = await fetch(`${USER_SEARCH_IP_API_URL}&ipAddress=${address}`);
+		const data = await res.json();
+		return json({ status: '200', data });
+	}
+
+	if (address.match(DOMAIN_REGEX)) {
+		const res = await fetch(`${USER_SEARCH_IP_API_URL}&domain=${address}`);
+		const data = await res.json();
+		return json({ status: '200', data });
+	} else {
+		return json({ status: '400', data: 'Bad Request' });
+	}
 }
